aae73a7f0dcd69d658a814ed7304dab88fcf7a0d
MaulanaKholis <119468707+MaulanaKholis@users.noreply.github.com>
2024-02-13 17:02:13 +0700

    Update part2.py

--- part2.py
+++ part2.py
@@ -15,7 +15,6 @@ class part2_topo(Mininet):
         h2 = self.addHost("h2", mac="00:00:00:00:00:02", ip="10.0.0.2/24")
         h3 = self.addHost("h3", mac="00:00:00:00:00:03", ip="10.0.0.3/24")
         h4 = self.addHost("h4", mac="00:00:00:00:00:04", ip="10.0.1.3/24")
-        c1 = self.addController("c1", controller=RemoteController, ip="127.0.0.1", port=6633)
         self.addLink(h1, s1)
         self.addLink(h2, s1)
         self.addLink(h3, s1)
@@ -27,7 +26,7 @@ topos = {"part2": part2_topo}
 
 def configure():
     topo = part2_topo()
-    net = Mininet(topo=topo, controller=c1)
+    net = Mininet(topo=topo, controller=RemoteController)
     net.start()
 
     CLI(net)
